f9b6db76295f5378c4a516a631da21a667e85b32
Christophe Combelles <ccomb@free.fr>
2014-04-04 16:02:37 +0300

    Cleanly separate stages and notifications

---
+++ anytracker/mindmap/mindmap.py
@@ -0,0 +1,40 @@
+# coding: utf-8
+from openerp.osv import osv
+from openerp.osv import fields
+
+
+class Ticket(osv.Model):
+    _inherit = 'anytracker.ticket'
+    _columns = {
+        'id_mindmap': fields.char('ID MindMap', size=64),
+        'created_mindmap': fields.datetime('Created MindMap'),
+        'modified_mindmap': fields.datetime('Modified MindMap'),
+        'modified_openerp': fields.datetime('Modified OpenERP'),
+    }
+
+    def makeTreeData(self, cr, uid, ids, context=None):
+        '''Return all ticket of a tree so ordered'''
+        data_to_retrieve = ['description',
+                            'modified_mindmap',
+                            'child_ids',
+                            'rating_ids',
+                            'id_mindmap',
+                            'modified_openerp',
+                            'created_mindmap',
+                            'id',
+                            'name']
+
+        def makeRecursTree(ticket_branch):
+            ticket_ids = self.search(cr, uid, [('parent_id', '=', ticket_branch['id'])])
+            for ticket_id in ticket_ids:
+                if 'child' not in ticket_branch:
+                    ticket_branch['child'] = []
+                ticket_branch['child'].append(
+                    self.read(cr, uid, ticket_id, data_to_retrieve, context)
+                )
+                makeRecursTree(ticket_branch['child'][len(ticket_branch['child'])-1])
+        ticket_tree = []
+        for ticket_tree_data in self.read(cr, uid, ids, data_to_retrieve, context):
+            ticket_tree.append(ticket_tree_data)
+            ticket_tree_data = makeRecursTree(ticket_tree_data)
+        return ticket_tree
---
+++ anytracker/tests/test_anytracker.py
@@ -0,0 +1,93 @@
+from anybox.testing.openerp import SharedSetupTransactionCase
+
+
+class TestAnytracker(SharedSetupTransactionCase):
+
+    _module_ns = 'anytracker'
+
+    @classmethod
+    def initTestData(self):
+        super(TestAnytracker, self).initTestData()
+        cr, uid = self.cr, self.uid
+        self.ticket_mdl = self.registry('anytracker.ticket')
+        self.user = self.registry('res.users')
+
+        self.member_id = self.user.create(
+            cr, uid,
+            {'name': 'Member',
+             'login': 'member',
+             'groups_id': [(6, 0,
+                           [self.ref('anytracker.group_member'),
+                            self.ref('base.group_user')])]})
+        self.manager_id = self.user.create(
+            cr, uid,
+            {'name': 'Manager',
+             'login': 'manager',
+             'groups_id': [(6, 0,
+                           [self.ref('base.group_user'),
+                            self.ref('anytracker.group_manager')])]})
+        self.customer_id = self.user.create(
+            cr, uid,
+            {'name': 'Customer',
+             'login': 'customer',
+             'groups_id': [(6, 0,
+                           [self.ref('anytracker.group_customer')])]})
+
+    def test_name_search(self):
+        """
+            Test the name_search function in anytracker.ticket module.
+            We may be able to find a ticket from his name or number
+        """
+        cr, uid = self.cr, self.uid
+        # Create a ticket
+        project_id = self.ticket_mdl.create(
+            cr, uid,
+            {'name': 'Quickstart test',
+             'participant_ids': [(6, 0, [self.customer_id, self.member_id, self.manager_id])],
+             'method_id': self.ref('anytracker.method_quickstart')})
+        ticket_id = self.ticket_mdl.create(
+            cr, uid, {'name': 'Test simple ticket', 'parent_id': project_id, })
+
+        # get his number
+        ticket_number = self.ticket_mdl.read(cr, uid, ticket_id, ['number'])['number']
+
+        # Search by name
+        ticket_id_by_name = self.ticket_mdl.search(cr, uid, [('name', '=', 'Test simple ticket')])
+
+        # Search by ticket number
+        ticket_id_by_number = self.ticket_mdl.search(cr, uid, [('name', '=', ticket_number)])
+
+        # Check result
+        for i in range(len(ticket_id_by_name)):
+            self.assertEquals(ticket_id_by_name[i], ticket_id)
+
+        for j in range(len(ticket_id_by_number)):
+            self.assertEquals(ticket_id_by_number[j], ticket_id)
+
+    def test_disable_project(self):
+        """ Set a node as inactive, and check all subtickets are also inactive
+        """
+        cr, uid = self.cr, self.uid
+        # create a project
+        project_id = self.ticket_mdl.create(
+            cr, uid,
+            {'name': 'Quickstart test2',
+             'participant_ids': [(6, 0, [self.customer_id, self.member_id, self.manager_id])],
+             'method_id': self.ref('anytracker.method_quickstart')})
+        # create a sub node and 2 tickets
+        node_id = self.ticket_mdl.create(cr, uid, {'name': 'A node', 'parent_id': project_id, })
+        self.ticket_mdl.create(cr, uid, {'name': 'ticket1', 'parent_id': node_id, })
+        self.ticket_mdl.create(cr, uid, {'name': 'ticket2', 'parent_id': node_id, })
+        # check that we can find all the tickets of the project
+        self.assertEquals(
+            4, self.ticket_mdl.search(cr, uid, [('id', 'child_of', project_id)], count=True))
+        # set the project as inactive
+        self.ticket_mdl.write(cr, uid, project_id, {'active': False})
+        # check that we CANNOT find all the tickets of the project
+        self.assertEquals(
+            0, self.ticket_mdl.search(cr, uid, [('id', 'child_of', project_id)], count=True))
+        # set the project as active again
+        self.ticket_mdl.write(cr, uid, project_id, {'active': True})
+        # check that we can find again all the tickets of the project
+        self.assertEquals(
+            4, self.ticket_mdl.search(cr, uid, [('id', 'child_of', project_id)], count=True))
